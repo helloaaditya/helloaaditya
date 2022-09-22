e35872e71a63b3185a10a8a70eed8e71f6e296e0
ADITYA KUMAR PRAJAPATI <103834315+ADITYA-PRAJAPATI22@users.noreply.github.com>
2022-09-22 12:57:05 +0530

    brief

---
+++ main.js
@@ -0,0 +1,11 @@
+function myfunction() {
+  alert("You are going to download the Resume!");
+}
+
+//for Certificates
+function Certificates(){
+  alert("You are going to be redirected to the original certificate!");
+}
+
+
+
