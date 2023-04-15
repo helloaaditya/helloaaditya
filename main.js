12ab86d6cafe0eeee4b23f59ddb42ccf99323f1c
Aditya <aadityakum123@gmail.com>
2023-04-16 03:37:25 +0530

    updated

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 function myfunction2() {
-  alert("You are going to download the Resume!");
+  alert("You are going to download the Resume! (File size : 1.26 MB)");
 }
 
-//for Certificates
-function Certificates(){
-  alert("You are going to be redirected to the original certificate!");
-}
+
 
 
 
